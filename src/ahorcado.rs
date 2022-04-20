ffad3b8d1f92c27f6fd73d8954e6baa0b8492344
Juan-M-V <juan.viel@lambdaclass.com>
2022-04-20 16:23:28 -0300

    Translate code

--- src/ahorcado.rs
+++ src/ahorcado.rs
@@ -1,108 +1,108 @@
-pub mod ahorcado {
+pub mod hangman {
     #[derive(Default)]
-    pub struct Ahorcado {
-        palabra: String,
-        intentos: usize,
-        letras_incorrectas: [char; 5],
-        letras_correctas: Vec<char>,
+    pub struct Hangman {
+        word: String,
+        attempts: usize,
+        misses: [char; 5],
+        hits: Vec<char>,
     }
 
-    pub enum ResultadoIntento {
-        PuedeSeguir,
-        Gana,
-        Muere,
+    pub enum AttemptResult {
+        CanKeepPlaying,
+        Win,
+        Hanged,
     }
 
-    const LETRA_EN_BLANCO:char = '_';
+    const SYMBOL_LETTER_TO_COMPLETE:char = '_';
 
-    impl Ahorcado {
-        pub fn new(palabra: String) -> Ahorcado {
-            let vec_size = palabra.len(); // Calculo size antes.
-            Ahorcado { 
-                palabra, // Se queda con la palabra.
-                intentos: 0, 
-                letras_incorrectas: Default::default(), 
-                letras_correctas: vec![LETRA_EN_BLANCO; vec_size], // Debe ser un vector porque no se puede saber el tamaño durante la compilación.
+    impl Hangman {
+        pub fn new(word: String) -> Hangman {
+            let vec_size = word.len();
+            Hangman { 
+                word, 
+                attempts: 0, 
+                misses: Default::default(), 
+                hits: vec![SYMBOL_LETTER_TO_COMPLETE; vec_size],
             }
         }
 
-        fn probar_letra(&mut self, l:char) -> ResultadoIntento {
+        fn guess_letter(&mut self, l:char) -> AttemptResult {
             
-            if self.letras_correctas.contains(&l) || self.letras_incorrectas.contains(&l) {
-                return ResultadoIntento::PuedeSeguir;
+            if self.hits.contains(&l) || self.misses.contains(&l) {
+                return AttemptResult::CanKeepPlaying;
             }
 
-            let mut acerto_letra = false;
-            let mut quedan_letras = false;
+            let mut guessed_letter = false;
+            let mut missing_letters = false;
 
-            for (i, c) in self.palabra.chars().enumerate(){
+            for (i, c) in self.word.chars().enumerate(){
                 if c == l {
-                    self.letras_correctas[i] = l;
-                    acerto_letra = true;
+                    self.hits[i] = l;
+                    guessed_letter = true;
                 }
 
-                if self.letras_correctas[i] == LETRA_EN_BLANCO {
-                    quedan_letras = true;
+                if self.hits[i] == SYMBOL_LETTER_TO_COMPLETE {
+                    missing_letters = true;
                 }
             }
 
-            if !quedan_letras {
-                ResultadoIntento::Gana
-            } else if acerto_letra {
-                ResultadoIntento::PuedeSeguir
+            if !missing_letters {
+                AttemptResult::Win
+            } else if guessed_letter {
+                AttemptResult::CanKeepPlaying
             } else {
-                self.letras_incorrectas[self.intentos] = l;
-                self.intentos += 1;
+                self.misses[self.attempts] = l;
+                self.attempts += 1;
 
-                if self.intentos == 5 {
-                    ResultadoIntento::Muere
+                if self.attempts == 5 {
+                    AttemptResult::Hanged
                 } else {
-                    ResultadoIntento::PuedeSeguir
+                    AttemptResult::CanKeepPlaying
                 }
             }
         }
 
         pub fn texto_juego(&self) {
-            const TITULO:&str  = "Bienvenido al ahorcado de LAMBDA!";
-            const PALABRA:&str = "La palabra hasta el momento es:";
-            const INTENTOS:(&str, &str) = ("Te quedan", "intentos.");
-            const LETRAS:&str = "Letras usadas:";
+            const TITLE:&str  = "Bienvenido al hangman de LAMBDA!";
+            const WORD:&str = "La word hasta el momento es:";
+            const ATTEMPTS:(&str, &str) = ("Te quedan", "attempts.");
+            const LETTERS:&str = "Letras usadas:";
             const PROMPT:&str = "Ingresa una letra:";
 
 
-            let mut adivinada_vec:Vec<char> = Vec::<char>::new();
+            let mut guessed_vec:Vec<char> = Vec::<char>::new();
 
-            for l in &self.letras_correctas {
-                adivinada_vec.push(*l); // Se toma el iterador.
-                adivinada_vec.push(' ');
+            for l in &self.hits {
+                guessed_vec.push(*l);
+                guessed_vec.push(' ');
             }
-            adivinada_vec.pop();
+            guessed_vec.pop();
 
-            let adivinada:String = adivinada_vec.into_iter().collect();
+            let guessed:String = guessed_vec.into_iter().collect();
 
             
-            let len_incorrectas = self.letras_incorrectas.len();
-            let mut incorrectas_vec = vec![' '; len_incorrectas * 2 - 1];
+            let len_misses = self.misses.len();
+            let mut misses_vec = vec![' '; len_misses * 2 - 1];
 
-            for (i, l) in self.letras_incorrectas[..len_incorrectas - 1].into_iter().enumerate() {
-                incorrectas_vec[i * 2] = *l; 
+            for (i, l) in self.misses[..len_misses - 1].into_iter().enumerate() {
+                misses_vec[i * 2] = *l; 
             }
 
-            let incorrectas:String = incorrectas_vec.into_iter().collect();
+            let misses:String = misses_vec.into_iter().collect();
 
 
-            std::process::Command::new("clear").status().expect("Error abriendo el archivo."); // TODO: ver si este unwrap puede quedar.
-            println!("{}", TITULO);
-            println!("{} {:#?}", PALABRA, adivinada);
-            println!("{} {} {}", INTENTOS.0, 5 - self.intentos, INTENTOS.1);
-            println!("{} {}", LETRAS, incorrectas);
+            std::process::Command::new("clear").status().expect("Error when opening the file.");
+            println!("{}", TITLE);
+            println!("{} {:#?}", WORD, guessed);
+            println!("{} {} {}", ATTEMPTS.0, 5 - self.attempts, ATTEMPTS.1);
+            println!("{} {}", LETTERS, misses);
             println!("{}", PROMPT);
         }
 
-        pub fn entrada_usr(&mut self) -> ResultadoIntento {
+        pub fn entrada_usr(&mut self) -> AttemptResult {
             let mut v = String::from("");
-            std::io::stdin().read_line(&mut v).expect("Error leyendo la linea.");
-            self.probar_letra(v.as_bytes()[0] as char)
+            std::io::stdin().read_line(&mut v).expect("Error reading the line.");
+            self.guess_letter(v.as_bytes()[0] as char)
         }
     }
 }
